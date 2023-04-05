f6b1e989b8f9ef890ab8043aecac37659e7d6502
Felix Odete <felix.odette@gmail.com>
2023-04-05 23:38:39 +0300

    Add image placeholders for news/blog page

--- src/components/News.js
+++ src/components/News.js
@@ -10,7 +10,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date">09 mar, 2019</span>
-                                <img src="/images/blog-img.jpg" alt="service-image" />
+                                <img src="/assets/images/blog-img.jpg" alt="service-image" />
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>A place where start new
@@ -32,7 +32,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date meta__date-date2">07 mar, 2019</span>
-                                <img src="/images/blog-img5.jpg" alt="service-image"/>
+                                <img src="/assets/images/blog-img5.jpg" alt="service-image"/>
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>We can make a difference
@@ -54,7 +54,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date meta__date-date3">04 mar, 2019</span>
-                                <img src="/images/blog-img6.jpg" alt="service-image" />
+                                <img src="/assets/images/blog-img6.jpg" alt="service-image" />
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>Build school for poor
@@ -76,7 +76,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date meta__date-date4">30 feb, 2019</span>
-                                <img src="/images/blog-img7.jpg" alt="service-image" />
+                                <img src="/assets/images/blog-img7.jpg" alt="service-image" />
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>Learn how access to clean
@@ -98,7 +98,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date">26 feb, 2019</span>
-                                <img src="/images/blog-img8.jpg" alt="service-image" />
+                                <img src="/assets/images/blog-img8.jpg" alt="service-image" />
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>Do something crazy to
@@ -120,7 +120,7 @@ const News = () => {
                         <div className="recent-item">
                             <div className="recent__img">
                                 <span className="meta__date-date meta__date-date2">20 feb, 2019</span>
-                                <img src="/images/blog-img9.jpg" alt="service-image" />
+                                <img src="/assets/images/blog-img9.jpg" alt="service-image" />
                             </div>
                             <div className="news__content">
                                 <h3 className="news__content-title"><Link href="/single-news"><a>Together to help the
