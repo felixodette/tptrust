0d1f8d8d19a11353195fa5259b381b189d6f835f
Felix Odete <felix.odette@gmail.com>
2023-04-12 08:18:40 +0300

    gitflow-feature-stash: 21-create-community-library-page

--- src/pages/pre-school.js
+++ src/pages/pre-school.js
@@ -12,6 +12,7 @@ import Footer from "../components/Footer";
 import PreSchoolAbout from "../components/PreSchoolAbout";
 // import OurWorkInfoSection from "../components/OurWorkInfoSection";
 import '../data/data.json';
+import PreSchoolInfoSection from "../components/PreSchoolInfoSection";
 const PreSchoolPage = () => {
   const [data, setData] = useState([]);
 
@@ -29,10 +30,7 @@ const PreSchoolPage = () => {
     <Layout pageTitle="TP Trust | Pre-School">
       <NavOne />
       <PageHeader title="Pre-School" />
-      <PreSchoolAbout />
-      <AboutDetails title={"Our Approach"} description={"We run a free preschool for children like Evans in Kibera who don’t have the opportunity to attend a preschool.\n" +
-        "Our preschool provides a safe environment for children to play, explore and grow. Each day involves a fun-packed programme of play-based learning, circle games and sports. The kids receive a healthy breakfast and lunch each day and our social workers work closely with families to promote positive parenting at home."}/>
-      <AboutDetails title={"Our Impact"} description={"Evans is now in our preschool learning to count and read. He loves doing puzzles and building houses and cars out of playdough. He makes lots of noise with his friends as they play football on the playground. Evans is gaining the foundational knowledge that will help him to do his best in primary school."}/>
+      <PreSchoolInfoSection />
       <MixerAreaTwo />
       {/*<TeamArea />*/}
       {/*<FaqArea />*/}
