81275eb09fe8ace0fde3b000a78c52405f7d61cc
Felix Odete <felix.odette@gmail.com>
2023-04-12 08:43:20 +0300

    Linter fixes

--- src/pages/community-library.js
+++ src/pages/community-library.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
-import AboutDetails from "../components/AboutDetails";
+// import AboutDetails from "../components/AboutDetails";
 import MixerAreaTwo from "../components/MixerAreaTwo";
 // import TeamArea from "../components/TeamArea";
 // import FaqArea from "../components/FaqArea";
 // import ServiceArea from "../components/ServiceArea";
 // import ClientsLogoTwo from "../components/ClientsLogoTwo";
 import Footer from "../components/Footer";
-import PreSchoolAbout from "../components/PreSchoolAbout";
+// import PreSchoolAbout from "../components/PreSchoolAbout";
 // import OurWorkInfoSection from "../components/OurWorkInfoSection";
 import '../data/data.json';
-import OurWorkInfoSection from "../components/OurWorkInfoSection";
-import myJson from "../data/data.json";
+// import OurWorkInfoSection from "../components/OurWorkInfoSection";
+// import myJson from "../data/data.json";
 import CommunityLibraryInfoSection from "../components/CommunityLibraryInfoSection";
 const CommunityLibraryPage = () => {
   const [data, setData] = useState([]);
@@ -26,7 +26,7 @@ const CommunityLibraryPage = () => {
       console.log(data);
     }
     fetchData();
-  }, []);
+  }, [data]);
 
   return (
     <Layout pageTitle="TP Trust | Community Library">
--- src/pages/school-support.js
+++ src/pages/school-support.js
@@ -2,20 +2,20 @@ import React, { useState, useEffect } from 'react';
 import Layout from "../components/Layout";
 import NavOne from "../components/NavOne";
 import PageHeader from "../components/PageHeader";
-import AboutDetails from "../components/AboutDetails";
+// import AboutDetails from "../components/AboutDetails";
 import MixerAreaTwo from "../components/MixerAreaTwo";
 // import TeamArea from "../components/TeamArea";
 // import FaqArea from "../components/FaqArea";
 // import ServiceArea from "../components/ServiceArea";
 // import ClientsLogoTwo from "../components/ClientsLogoTwo";
 import Footer from "../components/Footer";
-import PreSchoolAbout from "../components/PreSchoolAbout";
+// import PreSchoolAbout from "../components/PreSchoolAbout";
 // import OurWorkInfoSection from "../components/OurWorkInfoSection";
 import '../data/data.json';
-import OurWorkInfoSection from "../components/OurWorkInfoSection";
-import myJson from "../data/data.json";
-import CommunityLibraryInfoSection from "../components/CommunityLibraryInfoSection";
-import SchoolSupport from "./school-support";
+// import OurWorkInfoSection from "../components/OurWorkInfoSection";
+// import myJson from "../data/data.json";
+// import CommunityLibraryInfoSection from "../components/CommunityLibraryInfoSection";
+// import SchoolSupport from "./school-support";
 import SchoolSupportInfoSection from "../components/SchoolSupportInfoSection";
 const SchoolSupportPage = () => {
   const [data, setData] = useState([]);
@@ -28,7 +28,7 @@ const SchoolSupportPage = () => {
       console.log(data);
     }
     fetchData();
-  }, []);
+  }, [data]);
 
   return (
     <Layout pageTitle="TP Trust | School Support">
