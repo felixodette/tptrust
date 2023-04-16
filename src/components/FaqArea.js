c59f14d95bde352ad24aef8351b7b6bd74ec6c94
Felix Odete <felix.odette@gmail.com>
2023-04-16 23:35:11 +0300

    gitflow-feature-stash: enable-accordion

--- src/components/FaqArea.js
+++ src/components/FaqArea.js
@@ -1,11 +1,35 @@
 import React, {Component} from 'react';
-// import Accordion from "accordion/src/accordion.mjs";
+// import Accordion from "accordion";
+
+import {Accordion} from "react-accessible-accordion";
+
 
 class FaqArea extends Component {
 
-    // componentDidMount() {
-    //     new Accordion('.accordion-container');
-    // }
+    componentDidMount() {
+        const accordionContainer = document.querySelector('.accordion-container');
+        if (accordionContainer) {
+            new Accordion(accordionContainer, {
+                allowMultipleExpanded: true,
+                allowZeroExpanded: true,
+                header: '.ac-q',
+                panelClass: 'ac-a',
+                panelGroupClass: 'ac',
+                panelText: '.accordion__content-desc',
+                transitionSpeed: 400,
+                transitionEasing: 'ease',
+                transitionOpen: {
+                    height: 'toggle',
+                    opacity: 'toggle',
+                    paddingTop: 'toggle',
+                    paddingBottom: 'toggle',
+                    marginTop: 'toggle',
+                    marginBottom: 'toggle'
+                }
+            })
+        }
+    }
+
 
     render() {
         return (
@@ -15,13 +39,13 @@ class FaqArea extends Component {
                         <div className="col-lg-6">
                             <div className="section-heading">
                                 <div className="section-icon">
-                                    <img src="/assets/images/section-icon.png" alt="section-icon" />
+                                    <img src="/assets/images/section-icon.png" alt="section-icon"/>
                                 </div>
                                 <h2 className="section__title">Have any Question?</h2>
                                 <p className="section__meta">frequently asked questions</p>
                             </div>
                             <div className="faq-img-box">
-                                <img src="/assets/images/faq-img.jpg" alt="" />
+                                <img src="/assets/images/faq-img.jpg" alt=""/>
                             </div>
                         </div>
                         <div className="col-lg-6">
@@ -29,7 +53,10 @@ class FaqArea extends Component {
                                 <div className="ac">
                                     <h2 className="ac-q" tabIndex="0">Make a difference in the life of a child</h2>
                                     <div className="ac-a">
-                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est metus. There are many variations of eration in some form.
+                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae
+                                            consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper
+                                            vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est
+                                            metus. There are many variations of eration in some form.
                                         </p>
                                     </div>
                                 </div>
@@ -37,7 +64,10 @@ class FaqArea extends Component {
                                 <div className="ac">
                                     <h2 className="ac-q" tabIndex="0">Let’s do the right thing now</h2>
                                     <div className="ac-a">
-                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est metus. There are many variations of eration in some form.
+                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae
+                                            consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper
+                                            vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est
+                                            metus. There are many variations of eration in some form.
                                         </p>
                                     </div>
                                 </div>
@@ -45,7 +75,10 @@ class FaqArea extends Component {
                                 <div className="ac">
                                     <h2 className="ac-q" tabIndex="0">Join your hand with us for a better life</h2>
                                     <div className="ac-a">
-                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est metus. There are many variations of eration in some form.
+                                        <p className="accordion__content-desc">Suspendisse finibus urna mauris, vitae
+                                            consequat quam blandit vel. Vestibulum leo ligula, molestie ullamcorper
+                                            vulputate vitae sodales commodo nisl. Nulla facilisi. Pellentesque est
+                                            metus. There are many variations of eration in some form.
                                         </p>
                                     </div>
                                 </div>
