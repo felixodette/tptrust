2d1497067ee381e639821f0d32a5f42dc2ed00e3
salobalo <salonbieridze@gmail.com>
2023-09-27 13:06:42 +0400

    fixed links

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -5,33 +5,50 @@ const galleryItems = [
     {
         id: 1,
         image: "/assets/images/gallery-img-transition.png",
-        video:"https://drive.google.com/file/d/1Mk92P_YJQQY5cbFvO5y_lBY-eoNnIgu-/preview",
+        video:"https://youtu.be/FmjBktNEKL8?si=ltiWVYiW16LzQDFi",
     },
     {
         id: 2,
         image: "/assets/images/gallery-img-TPFC.png",
-        video:"https://drive.google.com/file/d/19Ede8OFuDlLdjohmA4bIJxL8P7cwgswD/preview",
+        video:"https://youtu.be/f9D8RshMq4Y?si=QgoUuiJh9UPtihQ8",
     },
     {
         id: 3,
         image: "/assets/images/gallery-img-TP's-Journey.png",
-        video:"https://drive.google.com/file/d/1FbkzodAATn0mN7-3HbUHYc6kNfLo8rHQ/preview",
+        video:"https://youtu.be/MyEHJKGYY5g?si=Va_FA5QmFcfbUKuM",
     },
     {
         id: 4,
         image: "/assets/images/gallery-img-feeding.png",
-        video:"https://drive.google.com/file/d/11geootR1nSUsYy9aqAhbFMW_klDNOJwv/preview",
+        video:"https://youtu.be/j9unrj2VtRY?si=cDs6wkdJanouuhor",
     },
     {
         id: 5,
         image: "/assets/images/gallery-img-desciplenship.png",
-        video:"https://drive.google.com/file/d/1-S7ATvVpZc3WQKXvTg8OFrvOn5BqyKXu/preview",
+        video:"https://youtu.be/IZXsZpv4558?si=Ss67pkU2rjGDSdZ_",
     },
     {
         id: 6,
         image: "/assets/images/gallery-img-anniversary.png",
-        video:"https://drive.google.com/file/d/1Gi6kDCaWR0eNbqFVeO2WiFv0awV5AppR/preview"
+        video:"https://www.youtube.com/watch?v=WVr17--kixo"
     },
+
+    {
+        id: 7,
+        image: "/assets//images/gallery-img10.jpg",
+        video:"/assets/images/gallery-img10.jpg"
+    },
+    {
+        id: 8,
+        image: "/assets//images/gallery-img10.jpg",
+        video:"/assets/images/gallery-img10.jpg"
+    },
+    {
+        id: 9,
+        image: "/assets//images/gallery-img10.jpg",
+        video:"/assets/images/gallery-img10.jpg"
+    }
+
 ];
 
 const Gallery = () => {
@@ -66,28 +83,6 @@ const Gallery = () => {
                         </div>
                     </div> 
                     ))}
-                    
-                    <div className="col-lg-4 col-sm-6">
-                        <div className="gallery-item">
-                            <img src="/assets//images/gallery-img10.jpg" alt="" />
-                                <a href="/assets/images/gallery-img10.jpg" className="glightbox">
-                                    <span className="gallery-icon"></span></a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6">
-                        <div className="gallery-item">
-                            <img src="/assets/images/gallery-img11.jpg" alt="" />
-                                <a href="/assets/images/gallery-img11.jpg" className="glightbox">
-                                    <span className="gallery-icon"></span></a>
-                        </div>
-                    </div>
-                    <div className="col-lg-4 col-sm-6">
-                        <div className="gallery-item">
-                            <img src="/assets//images/gallery-img12.jpg" alt="" />
-                                <a href="/assets/images/gallery-img12.jpg" className="glightbox">
-                                    <span className="gallery-icon"></span></a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
