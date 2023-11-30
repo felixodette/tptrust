653dfc5ee4d2489dd7e1443ded3df201c4fc3879
salome nebieridze <salo@192.168.0.121>
2023-11-30 15:22:36 +0400

    Updated fundraising campaign details on cause page: modified data.json to reflect the latest information for the ongoing campaign.

--- src/components/CausesItem.js
+++ src/components/CausesItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const CausesItem = ({ itemVal, causeTitle, causePreview, goal, raised, id, img }) => {
+const CausesItem = ({ itemVal, causeTitle, causePreview, donatePound,donateKsh, mpesaDetails, id, img }) => {
     return (
         <div className="col-lg-4">
             <div className="blog-content">
@@ -13,8 +13,9 @@ const CausesItem = ({ itemVal, causeTitle, causePreview, goal, raised, id, img }
                         <h3 className="blog__title"><Link to={`/causes-detail/${id}`}><a href="#/">{causeTitle}</a></Link></h3>
                         <p className="blog__desc">{causePreview}</p>
                         <ul className="blog__list">
-                            <li><i className="icon-target"></i> Goal: <span>${goal}</span></li>
-                            <li><i className="fa fa-line-chart"></i> Raised: <span>{raised}</span></li>
+                            <li><i className="icon-target"></i> Donate: <span>Ksh {donateKsh} /</span> <span>£{donatePound}</span></li>
+                            <li><i className="fa fa-line-chart"></i> MPESA details: <span>Paybill: {mpesaDetails.paybill}; Account:{mpesaDetails.account}</span>
+                            </li>
                         </ul>
                         <a href="https://www.globalgiving.org/donate/4620/the-turning-point-trust/" target="_blank" className="theme-btn">donate now</a>
                     </div>
