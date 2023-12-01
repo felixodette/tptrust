4112ed459c63b38a35e52e281aaceda34e932cee
salome nebieridze <salo@192.168.0.121>
2023-12-01 14:01:30 +0400

    Fix  cards heights on causes page to make them equally

--- src/components/CausesItem.js
+++ src/components/CausesItem.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 const CausesItem = ({ itemVal, causeTitle, causePreview, donatePound,donateKsh, mpesaDetails, id, img }) => {
     return (
-        <div className="col-lg-4">
-            <div className="blog-content">
-                <div className={`blog-item blog-item${itemVal}`}>
+        <div className="col-lg-6">
+            <div className="blog-content d-flex h-100">
+                <div className={`blog-item blog-item${itemVal} card flex-fill`}>
                     <div className="blog-img">
                         <img src={img} alt="" />
                     </div>
-                    <div className="blog-inner-content">
+                    <div className="blog-inner-content card-body">
                         <h3 className="blog__title"><Link to={`/causes-detail/${id}`}><a href="#/">{causeTitle}</a></Link></h3>
                         <p className="blog__desc">{causePreview}</p>
                         <ul className="blog__list">
                             <li><i className="icon-target"></i> Donate: <span>Ksh {donateKsh} /</span> <span>£{donatePound}</span></li>
-                            <li><i className="fa fa-line-chart"></i> MPESA details: <span>Paybill: {mpesaDetails.paybill}; Account:{mpesaDetails.account}</span>
+                            <li><i className="fa fa-line-chart"></i> MPESA: <span>Paybill: {mpesaDetails.paybill}; {mpesaDetails.account}</span>
                             </li>
                         </ul>
                         <a href="https://www.globalgiving.org/donate/4620/the-turning-point-trust/" target="_blank" className="theme-btn">donate now</a>
